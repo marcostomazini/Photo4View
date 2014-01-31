7a6233c84e2da6b8969455634caa07b71dc39883
unknown <marcos.tomazini@gmail.com>
2014-01-30 23:40:52 -0200

    1* Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+
+function getPhoto() {
+	var getphoto = new MozActivity({
+	  name: "pick",
+	  data: {
+		type: ["image/png", "image/jpg", "image/jpeg"]
+	  }
+	});
+
+	getphoto.onsuccess = function () {
+	  qrcode.callback = read;
+	  var img = document.createElement("img");
+	  if (this.result.blob.type.indexOf("image") != -1) {
+		img.src = window.URL.createObjectURL(this.result.blob);
+		qrcode.decode(img.src);
+	  }
+	};
+
+	getphoto.onerror = function () { 
+		alert("deu erro");
+	};	
+}	
+
+function read(qr)
+{
+	if (qr.length == 6) {
+		document.location = "https://photo4ffx.herokuapp.com/cliente/id.html#" + qr;
+	} else {
+		alert("QR Code Inválido");
+	}
+}	
